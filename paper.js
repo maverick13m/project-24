b971c35075e6f7594b10847a6f1351324db0c8b8
maverick13m <71173739+maverick13m@users.noreply.github.com>
2020-10-16 18:55:28 +0530

    Add files via upload

--- paper.js
+++ paper.js
@@ -2,26 +2,25 @@ class paper {
     constructor(x,y,r){
         var options = {
             isStatic:false,
-            restitution:0.3,
+            restitution:0.5,
             friction:0.5,
             density:1.2
-        }  
+        }; 
         this.x=x;
         this.y=y;
-        this.r=r 
-        this.body = Bodies.circle(this.x,this.y,this.r/2,options)
-        World.add(World, this.body)
-    }
+        this.r=r; 
+        this.body = Bodies.circle(this.x,this.y,this.r,options)
+        World.add(world, this.body);
+    };
     display() { 
-        var paperpos = this.body.position
-    
-        Push()
-        translate(paperpos.x, paperpos.y)
-        rectMode(CENTER)
-        strokeWeight(3)
-        Fill(255,0,255) 
-        ellipse(0,0,this.r,this.r)       
-        Pop()
+        var paperpos = this.body.position;
+        push();
+        translate(paperpos.x, paperpos.y);
+        rectMode(CENTER);
+        strokeWeight(3);
+        fill(255,0,255) ;
+        ellipse(0,0,this.r,this.r);
+        pop()
 
     }
 
--- sketch.js
+++ sketch.js
@@ -5,40 +5,19 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
+var engine, world;
 
 function preload(){
 	
 }
 
 function setup() {
+  engine =Engine.create()
+  world = engine.world
 	createCanvas(800, 800);
-
-	groundSprite = createSprite(400,800,width,10)
-	groundSprite . shapeColor = color("yellow")
-
-    boxside1 = createSprite(500,720,20,100)
-	boxside1 . shapeColor = color("white")
-	
-	boxside2 = createSprite(300,720,20,100)
-	boxside2 . shapeColor = color("white")
-
-	boxbottom = createSprite(400,770,200,20)
-	boxbottom . shapeColor = color("white")
-
-	engine = Engine.create();
-	world = engine.world;
-
-	 boxbottom = Bodies.rectangle(400, 770, 200, 20 , {isStatic:true} );
-	 World.add(world, boxbottom);
-	 
-	 boxside1 = Bodies.rectangle(500, 720, 20, 100 , {isStatic:true} );
-	 World.add(world, boxside1);
-	 
-	 boxside2 = Bodies.rectangle(300, 720, 20, 100 , {isStatic:true} );
-     World.add(world, boxside2);
-
-
-
+	ground = new Ground(400,620,width,20)
+  paperObject = new paper(100,90,50)
+  dustbin = new Dustbin(600,600)
 	Engine.run(engine);
   
 }
@@ -48,17 +27,14 @@ function draw() {
   rectMode(CENTER);
   Engine.update(engine)
   background(0);
-
-  drawSprites();
- keypress();
+  ground.display();
+  paperObject.display();
+  dustbin.display();
 }
 
-function keypress(){
+function keyPressed(){
     if (keyCode == UP_ARROW) {
-        Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
-
+        Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-185});
 	}
-
-
 }
 
